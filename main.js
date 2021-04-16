c2214aca32667a2f307716545d8d8126b5516e95
ciobanuradu96 <ciobanu.radu96@gmail.com>
2021-04-16 17:32:44 +0300

    all inputs are reacive results area almost done

--- main.js
+++ main.js
@@ -2,7 +2,6 @@
 //// Function to create towWay data binding between the input range and input number
 twoWay = function (e) {
     let elem = document.getElementsByClassName(e.target.className);
-    console.log(elem)
     for (var key in elem) {
         elem[key].value = e.currentTarget.value;
     }
@@ -10,55 +9,98 @@ twoWay = function (e) {
 
 /// Function to change to the next card the card will get the .current class wich will bring it
 ///forword
-const nextSlide = () => {
-    const current = document.querySelector('.current')
-    current.classList.remove('current')
+// const nextSlide = () => {
+//     const current = document.querySelector('.current')
+//     current.classList.remove('current')
 
-    if (current.nextElementSibling) {
-        current.nextElementSibling.classList.add('current')
-        console.log('test1')
-    } else {
-        cards[0].classList.add('current')
-        console.log('test2')
+//     if (current.nextElementSibling) {
+//         current.nextElementSibling.classList.add('current')
+       
+//     } else {
+//         cards[0].classList.add('current')
+   
+//     }
+// }
+
+
+results=  ()=>{
+
+    let gender= document.querySelector('#genderSlide').value
+    let bodyFat= document.querySelector('#bodyFat')
+    ageValue.value = ageSlide.value
+    let weight=weightValue.value = weightSlide.value
+    let height=heightValue.value = heightSlide.value
+    let bmr= document.querySelector('#bmr')
+    let bmi=document.querySelector('#bmi')
+    let activityLvl=document.querySelector('#activityLvl')
+    let tdee=document.querySelector('#tdee')
+
+    bmi.innerHTML = (weight/Math.pow(height/100,2))
+    
+    if (bodyFat.value!=""){
+        //Katch
+        let lbm= weight-(weight*bodyFat.value*0.001)
+         bmr.innerHTML=370+21.6*lbm
+    }else{
+        //Mifflin
+        let s=0;
+        gender=="1"? s=5: s=-151;
+        bmr.innerHTML=(((10*weight)+(6.25*height)-(5*ageValue.value))+s)
+        
     }
-}
+    // activity level based on the option selected 
+    switch(activityLvl.value){
+        case "sedentary":
+             tdee.innerHTML=bmr.innerHTML*1.2
+            break;
+        case "light":
+            tdee.innerHTML=bmr.innerHTML*1.37
+            break;
+        case "moderate":
+            tdee.innerHTML=bmr.innerHTML*1.55    
+            break;
+        case "heavy":
+            tdee.innerHTML=bmr.innerHTML*1.725    
+            break; 
+        case "athlete":
+            tdee.innerHTML=bmr.innerHTML*1.9    
+            break; 
+    }
+    
 
+    }
 
-
+const ageSlide = document.querySelector('#ageSlide')
+const ageValue = document.querySelector('#ageValue')
+const weightSlide = document.querySelector('#weightSlide')
+const weightValue = document.querySelector('#weightValue')
+const heightSlide = document.querySelector('#heightSlide')
+const heightValue = document.querySelector('#heightValue')
 const cards = document.querySelectorAll('.card')
 const next = document.querySelectorAll('#next')
 const prv = document.querySelector('#prev')
-
-///
-const ageSlide = document.querySelector('#ageSlide')
-let ageValue = document.querySelector('#ageValue')
-ageValue.value = ageSlide.value
-
-/////
-
-const weightSlide = document.querySelector('#weightSlide')
-let weightValue = document.querySelector('#weightValue')
-weightValue.value = weightSlide.value
-
-//////
-
-const heightSlide = document.querySelector('#heightSlide')
-let heightValue = document.querySelector('#heightValue')
-heightValue.value = heightSlide.value
+let gender= document.querySelector('#genderSlide')
+let bodyFat= document.querySelector('#bodyFat')
+let activityLvl=document.querySelector('#activityLvl')
 
 
+results()
+////// Setting up 2 way data binding for each input 
+
+inputTowWays=[ageSlide,weightSlide,heightSlide,ageValue,weightValue,heightValue]
+inputTowWays.forEach(input => input.addEventListener('input',twoWay))
+
+inputs=[ageSlide,weightSlide,heightSlide,ageValue,weightValue,heightValue,gender,bodyFat,activityLvl]
+inputs.forEach(input=>input.addEventListener('input',results))
 
 
-next.forEach(element => {
-    console.log(element)
-    element.addEventListener('click', nextSlide);
-});
+// next.forEach(element => {
+//     element.addEventListener('click', nextSlide);
+// });
 
-ageSlide.addEventListener('input', twoWay)
-ageValue.addEventListener('input', twoWay)
 
-weightSlide.addEventListener('input', twoWay)
-weightValue.addEventListener('input', twoWay)
 
-heightSlide.addEventListener('input', twoWay)
-heightValue.addEventListener('input', twoWay)
+
+
+
+
